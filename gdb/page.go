6f4ac39aa4a112d389be9544012a0c0b4548ff8c
Martin Skou Drewes <msj@infoserv.dk>
2019-02-20 08:04:14 +0100

    update

--- gdb/page.go
+++ gdb/page.go
@@ -2,24 +2,117 @@ package gdb
 
 import (
     "fmt"
+    "strings"
+    "math/rand"
+    "time"
 )
 
 type Page struct {
 	Title    string
 	Name     string
+	Slug     string
+	Content  string
+	Parent   *Page
 	Children *[]Page
+}
+
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randString(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
+}
+
+func (p Page) Print(index int, level int) {
+    fmt.Printf("%s %d Title: %s\n", strings.Repeat("  ",level), index, p.Title)
+    fmt.Printf("%s %d Name:  %s\n", strings.Repeat("  ",level), index, p.Name)
+    for i,c := range *p.Children {
+        c.Print(i,level+1)
+    }
+    return
+}
+
+
+func (p Page) Apply(fn func(Page)) {
+	fn(p)
+    for _,c := range *p.Children {
+        c.Apply(fn)
+    }
+    return
+}
+
+func (p Page) AbsSlug() string {
+	slug := p.Slug
+	parent := p.Parent
+	if parent != nil {
+		for parent != nil {
+			slug = strings.Join( []string{parent.Slug , slug} , "/")
+			parent = parent.Parent
+		}
+	} else {
+		slug = "/"
+	}
+	return slug
 }
 
 func ExampleSite () Page {
     p := Page{Title: "Frontpage",
-		Name: "Frontpage",
-		Children: &[]Page{
+			  Name: "Frontpage",
+			  Slug: "/",
+			  Children: &[]Page{
 			{Title: "Products", Children: &[]Page{{Title: "Prod1", Children: &[]Page{}},
 				{Title: "Prod2", Children: &[]Page{}}}},
 			{Title: "About us", Children: &[]Page{}}}}
     return p
 }
 
+func TestRnd() {
+    var m map[int]int
+    m = make(map[int]int,0)
+    for i := 0; i < 100000; i++ {
+        x:=rand.Intn(10)
+        //println(x)
+        m[x]+=1
+    }
+    for k := range m {
+        println(k,m[k])
+    }
+}
+
+func RandomPages (max_pages int, parent *Page) *[]Page {
+    //ap := make([]Page,0)
+    var ap []Page
+	if max_pages>0 {
+	    pages := rand.Intn(max_pages)
+	    for i := 0; i < pages; i++ {
+	        t:=strings.Title(randString(rand.Intn(5)+2))
+	        p:= Page{Title: t,
+	                 Name: t,
+					 Slug: strings.ToLower(t),
+					 Content: randString(80),
+					 Parent: parent} // &[]Page{}}
+			p.Children = RandomPages(max_pages-2,&p)
+	        ap=append(ap,p)
+	    }
+	}
+    return &ap //[]Page{ p }
+}
+
+
+func RandomSite () Page {
+    p := Page{Title: "Frontpage",
+			  Name: "Frontpage",
+			  Slug: "",
+			  Content: "Blah blak...",
+			  Parent: nil}
+	p.Children = RandomPages(10,&p)
+    return p
+}
+
 func init() {
-    fmt.Printf("data.go init\n")
+    rand.Seed(time.Now().UnixNano())
 }
--- server.go
+++ server.go
@@ -35,13 +35,13 @@ type TemplateRenderer struct {
 // Render renders a template document
 func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 
-	root_page := gdb.ExampleSite()
+//	root_page := gdb.ExampleSite()
 
 	// Check if data is a map with string keys (type switch)
 	if viewContext, isMap := data.(map[string]interface{}); isMap {
-        viewContext["current"] = root_page
-        viewContext["pages"] = root_page
-        fmt.Printf("%#v\n", data)
+//        viewContext["current"] = root_page
+//        viewContext["pages"] = root_page
+        fmt.Printf("%#v\n", viewContext)
 	}
 	if debug {
 		t := template.Must(template.ParseGlob(templateGlob))
@@ -54,7 +54,7 @@ func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c 
 
 
 
-func main() {
+func run() {
 
 	//	db := initDB("storage.db")
 	//	migrate(db)
@@ -75,26 +75,57 @@ func main() {
         }
     }
 
+	root_page := gdb.RandomSite()
+
+	root_page.Apply(func(p gdb.Page) {
+		println(p.Name, p.Slug, p.AbsSlug())
+		e.GET(p.AbsSlug(), func(c echo.Context) error {
+			//	return c.String(http.StatusOK, p.AbsSlug())
+			c.Render(http.StatusOK, "demo1.html", map[string]interface{}{"pages":root_page, "current": p})
+			return nil
+		})
+	})
+
+
 	// Named route "foobar"
 	e.GET("/something", func(c echo.Context) error {
 		c.Render(http.StatusOK, "demo1.html", map[string]interface{}{
-			"name": "Dolly!", "menu": [2]string{"test", "test2"},
-		})
+			"pages":root_page})
         return nil
 	}).Name = "foobar"
 
-	e.Static("/", "assets")
-	e.File("/favicon.ico", "assets/favicon.ico")
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
+	e.GET("/z*", func(c echo.Context) error {
+		fmt.Printf("%+v\n", c)
+		return c.String(http.StatusOK, "/users/1/files/*")
+	})
+
 	e.File("/d/", "templates/demo1.html")
+	e.File("/favicon.ico", "assets/favicon.ico")
+	e.Static("/", "assets")
 	//	e.File("/", "public/index.html")
 	//	e.GET("/tasks", handlers.GetTasks(db))
 	//	e.PUT("/tasks", handlers.PutTask(db))
 	//	e.DELETE("/tasks/:id", handlers.DeleteTask(db))
 
 	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func test() {
+    //run()
+    //gdb.TestRnd()
+    root_page := gdb.RandomSite()
+    root_page.Print(0,0)
+	//fmt.Printf("%+v\n", root_page)
+}
+
+func main() {
+    run()
+    //for i := 0; i < 10; i++ {
+    //    test()
+    //}
 }
